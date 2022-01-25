e3f19bfa0f11aef9a01e6eb53cc91cc463681535
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-01-25 12:18:18 -0300

    Add div to product makers

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 
-import {cartInterceptor, mainInterceptor} from './src/script/features/cartManager.js'
+import { cartInterceptor, mainInterceptor } from './src/script/features/cartManager.js'
 const main = document.getElementById('mainProducts')
 const cart = document.getElementById('itemsToBuy')
 
-export default function creatingShowCase(item){ 
+export default function creatingShowCase(item) {
     item.forEach(item => {
-    
-    const {categoria, descricao, imagem, nome, preco, id} = item;
-    
-    const img = document.createElement("img");
-    const cat = document.createElement("span");
-    const name = document.createElement("h2");
-    const description = document.createElement("p");
-    const price = document.createElement("p");
-    const button = document.createElement("button")
 
-    img.src                 =  imagem;
-    cat.innerText           =  categoria;
-    name.innerText          =  nome;
-    description.innerText   =  descricao;
-    price.innerText         =  preco;
-    button.innerText        =  "icone"
-    button.id               =  id
+        const { categoria, descricao, imagem, nome, preco, id } = item;
 
-    main.appendChild(img);
-    main.appendChild(cat);
-    main.appendChild(name);
-    main.appendChild(description);
-    main.appendChild(price);
-    main.appendChild(button)
-    document.body.appendChild(main);
+        const div = document.createElement("div")
+        const img = document.createElement("img");
+        const cat = document.createElement("span");
+        const name = document.createElement("h2");
+        const description = document.createElement("p");
+        const price = document.createElement("p");
+        const button = document.createElement("button")
+
+        img.src = imagem;
+        cat.innerText = categoria;
+        name.innerText = nome;
+        description.innerText = descricao;
+        price.innerText = preco;
+        button.innerText = "icone"
+        button.id = id
+
+        div.appendChild(img);
+        div.appendChild(cat);
+        div.appendChild(name);
+        div.appendChild(description);
+        div.appendChild(price);
+        div.appendChild(button)
+        div.appendChild(button)
+        main.appendChild(div)
     })
 }
 
